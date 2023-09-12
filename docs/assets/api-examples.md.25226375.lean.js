a7345013699207b51141eae810f24e45a67cad7e
ALEX hu <798921360@qq.com>
2023-09-12 15:14:18 +0800

    router

---
+++ docs/assets/api-examples.md.25226375.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as e,Q as o}from"./chunks/framework.419948d5.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"api-examples.md","filePath":"api-examples.md"}'),p={name:"api-examples.md"},n=o("",1),t=[n];function l(c,r,i,_,d,E){return a(),e("div",null,t)}const h=s(p,[["render",l]]);export{m as __pageData,h as default};
